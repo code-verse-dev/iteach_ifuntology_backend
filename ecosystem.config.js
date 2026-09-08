50e9f1ba4a04337c9d0eda56519502555e82f955
Umair6590 <156321849+Umair6590@users.noreply.github.com>
2026-09-08 13:05:18 +0500

    Add deploy files from template

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "iteach_ifuntology_backend",
+      script: "dist/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "live",
+      },
+    },
+  ],
+};
